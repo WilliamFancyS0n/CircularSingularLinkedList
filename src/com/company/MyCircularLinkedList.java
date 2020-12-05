b54c5fdd6598297d27ec2338ac29e72410cc15ad
William Alger <35159547+WilliamFancyS0n@users.noreply.github.com>
2020-12-05 04:17:08 -0500

    progress!

--- src/com/company/MyCircularLinkedList.java
+++ src/com/company/MyCircularLinkedList.java
@@ -1,53 +1,46 @@
 /*
 Name: William Alger
+Date: Saturday, December 5th, completed at 1:54 AM :)
+Purpose of program:
+    This is an implementation of a circular singularly linked list with tail pointer.
+
+
+
+
  */
 package com.company;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Objects;
+import java.util.*;
 import java.lang.StringBuilder;
 
 public class MyCircularLinkedList<E> implements MyList<E> {
     private static int size = 0;
     Node<E> tail = null;
-    public static void main(String[] args)
-    {
-//        MyCircularLinkedList<String> list = new MyCircularLinkedList<>();
-//        list.add("1");
-//        list.add("2");
-//        list.add("3");
-//        System.out.println(list);
-//        list.remove(1);
-//        System.out.println(list);
-    }
 
+
+//    public static void main(String[] args) // main to test methods individually
+//    {
+//
+//    }
 
     public MyCircularLinkedList()
     {
 
     }
-
     public MyCircularLinkedList(E[] objects)
     {
         for (int i = 0; i < objects.length; i++)
             add(objects[i]);
     }
-
-
     @Override
     public int size()
     {
         return size;
     }
-
     @Override
     public boolean contains(Object o)
     {
-        if (size == 0)
-        {
-            return false;
-        }
+        if (size == 0) return false;
         else {
             Node<E> current = tail.next;
             for (int i = 0; i < size; i++) {
@@ -58,11 +51,7 @@ public class MyCircularLinkedList<E> implements MyList<E> {
         }
     }
 
-    @Override
-    public Iterator<E> iterator()
-    {
-        return new CircularLinkedListIterator();
-    }
+
 
     @Override
     public void clear()
@@ -70,8 +59,6 @@ public class MyCircularLinkedList<E> implements MyList<E> {
         size = 0;
         tail = null;
     }
-
-
     private static class Node<E> {
         E element;
         Node<E> next;
@@ -79,15 +66,20 @@ public class MyCircularLinkedList<E> implements MyList<E> {
         public Node(E element) {
             this.element = element;
         }
-    }
 
+        @Override
+        public boolean equals(Object o){
+            return element.equals(o);
+        }
+    }
     @Override
     public void add(int index, E e) {
         if (index == 0)
             addFirst(e);
-        else if (index >= size) {
+        else if (index == size)
             addLast(e);
-        }
+        else if (index > size || index < 0)
+            throw new IndexOutOfBoundsException();
         else {
             Node<E> current = tail; //start at first node
             for (int i = 0; i < index; i++) current = current.next;
@@ -98,7 +90,6 @@ public class MyCircularLinkedList<E> implements MyList<E> {
             size++;
         }
     }
-
     public void addFirst(E e) {
         Node<E> node = new Node<>(e);
         if (tail == null) {
@@ -109,11 +100,9 @@ public class MyCircularLinkedList<E> implements MyList<E> {
             Node<E> tmp = tail.next; //store link to original first node
             tail.next = node; //link tail to new node
             node.next = tmp; //link new node to previously first node
-
         }
         size++;
     }
-
     public void addLast(E e) {
         Node<E> node = new Node<>(e);
 
@@ -133,18 +122,9 @@ public class MyCircularLinkedList<E> implements MyList<E> {
     @Override
     public E remove(int index)
     {
-        if (index < 0 || index >= size) {
-            return null;
-        }
-        else if (index == 0)
-        {
-            return removeFirst();
-
-        }
-        else if (index >= size-1)
-        {
-            return removeLast();
-        }
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        else if (index == 0) return removeFirst();
+        else if (index == size-1) return removeLast();
         else
         {
             Node<E> current = tail.next;
@@ -155,11 +135,8 @@ public class MyCircularLinkedList<E> implements MyList<E> {
             return tmp.element;
         }
     }
-
-
     public E removeFirst() {
-        if (size == 0)
-            throw new NoSuchElementException();
+        if (size == 0) throw new NoSuchElementException();
         else {
             Node<E> tmp = tail.next; //store the first node as temp
             tail.next = (tail.next).next; //connect the tail.next link to the node after the first
@@ -167,10 +144,8 @@ public class MyCircularLinkedList<E> implements MyList<E> {
             return tmp.element; //return first element
         }
     }
-
     public E removeLast() {
-        if (size == 0)
-            throw new NoSuchElementException();
+        if (size == 0) throw new NoSuchElementException();
         else if (size == 1) { //if there is only one node in our linked list
             Node<E> tmp = tail;
             tail = null;
@@ -187,10 +162,9 @@ public class MyCircularLinkedList<E> implements MyList<E> {
             return tmp.element; //return removed element
         }
     }
-
     @Override
     public E get(int index) {
-        if (size == 0)
+        if (size == 0 || index < 0  || index > size-1)
             throw new IndexOutOfBoundsException();
         else if (index == size-1)
             return tail.element;
@@ -200,59 +174,52 @@ public class MyCircularLinkedList<E> implements MyList<E> {
             return current.element;
         }
     }
-
     @Override
-    public int indexOf(Object e) {
-        if (size > 0) { // if our list isn't empty
+    public int indexOf(Object e)
+    {
+        if (size > 0) {
             int index = 0;
             Node<E> current = tail.next;
-            while (current != e) { //while our current object isn't the object we're looking for
-                current = current.next;
+            while (index < size) {
+                if (current.equals(e))
+                    return index;
                 index++;
+                current = current.next;
             }
-            if (current == e) return index; //if we found the object, return its index
-            else return -1; //if not found
         }
-        else return -1; //if list is empty
+        return -1;
     }
-
     @Override
     public int lastIndexOf(E e) {
+        PriorityQueue<Integer> list = new PriorityQueue<>(Collections.reverseOrder());
         if (size > 0) {
-            return 1;
+            int index = 0;
+            Node<E> current = tail.next;
+            while (index < size) {
+                if (current.equals(e))
+                    list.add(index);
+                index++;
+                current = current.next;
+            }
+            if (!list.isEmpty())
+                return list.poll();
         }
-        else return -1;
+       return -1;
     }
-
-
     @Override
     public E set(int index, E e)
     {
-        if (size == 0) { // if list is empty, add the new object as the first object to a new list;
-            addFirst(e);
-            return null;
-        }
-        else if (index > size) {
-            addLast(e);
-            return null;
-        }
+        if (index < 0 || index > size-1) throw new IndexOutOfBoundsException();
         else {
-            Node<E> node = new Node<>(e); //create a new node
-            Node<E> current = tail.next; //set the pointer to the tail
-            for (int i = 0; i < index; i++) { //iterate to the node before the index we're replacing/setting
-                current = current.next;
-            }
-            Node<E> tmp = current.next; // copy the node we're replacing
-            current.next = node; // point the previous node to our new node
+            Node<E> current = tail.next;
+            Node<E> node = new Node<>(e);
+            for (int i = 1; i < index; i++) current = current.next;
+            Node<E> tmp = current.next; //copy the node we're replacing
+            current.next = node;
             node.next = tmp.next;
             return tmp.element;
         }
-
-
-
     }
-
-
     public String toString()
     {
         StringBuilder result = new StringBuilder("[");
@@ -267,48 +234,54 @@ public class MyCircularLinkedList<E> implements MyList<E> {
                 result.append(", ");
             }
             current = current.next;
-
-
         }
         result.append("]");
         return result.toString();
     }
-
-
     public E getFirst() {
         if (size == 0) throw new NoSuchElementException();
         else return tail.next.element;
     }
-
     public E getLast() {
         if (size == 0) throw new NoSuchElementException();
         else return tail.element;
     }
+
+    @Override
+    public Iterator<E> iterator()
+    {
+        return new CircularLinkedListIterator();
+    }
+
     private class CircularLinkedListIterator implements java.util.Iterator<E> {
 
         private Node<E> current = tail.next;
-
+        boolean isCalled = false;
+        E lastReturned;
         @Override
         public boolean hasNext()
         {
            return current.next != tail.next;
         }
-
         @Override
         public E next()
         {
             E e = current.element;
             current = current.next;
+            isCalled = true;
+            lastReturned = e;
             return e;
         }
+        @Override
+        public void remove()
+        {
+           if (isCalled == false) throw new IllegalStateException();
+           else {
+               MyCircularLinkedList.this.remove(lastReturned);
+           }
+           isCalled = false;
+        }
     }
-
-
-
-
-
-
-
 }
 
 
